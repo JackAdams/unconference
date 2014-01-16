12a1ae27d7857f1f6f1f3e745025ff7b7dd6921b
JackAdams <brent_abrahams@yahoo.com>
2014-01-16 13:39:36 +0000

    Initial commit

---
+++ collections.js
@@ -0,0 +1,4 @@
+Unconferences = new Meteor.Collection('unconferences');
+Topics = new Meteor.Collection('topics');
+Rooms = new Meteor.Collection('rooms');
+Timeslots = new Meteor.Collection('timeslots');
